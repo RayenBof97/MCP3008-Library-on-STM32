b3b3f4c7a24062b5557449cdc1f091055512912e
Bouafif Rayen <rayen.bouafif@insat.ucar.tn>
2025-02-05 22:34:18 +0100

    Added Errors Handling

--- Core/Inc/MCP_Driver.h
+++ Core/Inc/MCP_Driver.h
@@ -16,16 +16,37 @@ typedef struct{
 	SPI_HandleTypeDef* hspi;									/*!<Assign The SPI Over which The communication is being held */
 	GPIO_TypeDef *CS_gpio;										/*!<Assign The GPIO Over which CS is connected , Macros in @addtogroup Peripheral_declaration */
 	uint16_t CS_PinNumber;										/*!<Assign The Pin Number  Over which CS is connected*/
+	uint8_t MCP_Mode;											/*!<Assign the MCP Mode , Macros in @Single/Diff Mode*/
 }MCP_Handler_t;
+
+
+/*
+ * @Single/Diff Mode
+ */
+#define MCP_SINGLE_ENDED_MODE	1
+#define MCP_DIFFERENTIAL_ENDED_MODE	0
+
+/*
+ * @MCP_Channels
+ */
+#define MCP_Channel_0	0
+#define MCP_Channel_1	1
+#define MCP_Channel_2	2
+#define MCP_Channel_3	3
+#define MCP_Channel_4	4
+#define MCP_Channel_5	5
+#define MCP_Channel_6	6
+#define MCP_Channel_7	7
+
 
 /**************************************************************************************************
  * 									APIs Supported by this driver
  *					For further informations, please check the functions definition
  **************************************************************************************************/
-void MCP_Init(MCP_Handler_t *Mcp_handle,SPI_HandleTypeDef* pspi,GPIO_TypeDef *pCS_gpio,uint16_t CS_PinNumber);
-void MCP_StopCommunication(MCP_Handler_t *Mcp_Handle);
-void MCP_StartCommunication(MCP_Handler_t *Mcp_Handle);
-void MCP_ResetCommunication(MCP_Handler_t *Mcp_Handle);
-uint16_t MCP_ReadChannel(MCP_Handler_t *Mcp_Handle, uint8_t channel);
+void MCP3008_Init(MCP_Handler_t *Mcp_handle,SPI_HandleTypeDef* hspi,GPIO_TypeDef *pCS_gpio,uint16_t CS_PinNumber,uint8_t MCP_Mode);
+void MCP3008_StopCommunication(MCP_Handler_t *Mcp_Handle);
+void MCP3008_StartCommunication(MCP_Handler_t *Mcp_Handle);
+void MCP3008_ResetCommunication(MCP_Handler_t *Mcp_Handle);
+uint16_t MCP3008_ReadChannel(MCP_Handler_t *Mcp_Handle, uint8_t channel);
 
 #endif /* INC_MCP_DRIVER_H_ */
--- Core/Src/MCP_Driver.c
+++ Core/Src/MCP_Driver.c
@@ -8,48 +8,120 @@
   * @date    04-05-2025
   *****************************************************************************
 * This File Also contain the APIs explanation in the comments
+* You application should include STM HAL.
+*
+*
 */
 /*INCLUDES*/
 #include "MCP_Driver.h"
 
-void MCP_Init(MCP_Handler_t *Mcp_handle,SPI_HandleTypeDef* pspi,GPIO_TypeDef *pCS_gpio,uint_t CS_PinNumber){
+/********************************************************************
+ * @fn				-  MCP3008_StartCommunication
+ *
+ * @brief			- Initialise the MCP Handler
+ *
+ * @param[in]		- Address of MCP_Handle (Declared in the application)
+ * @param[in]		- Address of spi_Handle (Declared in the application using HAL)
+ * @param[in]		- Address of GPIO used for Chip Select (Declared in the application)
+ * @param[in]		- Chip select Pin Number (Use the HAL Macros)
+ * #param[in]		- MCP Mode (@MCP_Mode)
+ *
+ * @return 			- NONE
+ *
+ * @note			- NONE
+ */
+void MCP3008_Init(MCP_Handler_t *Mcp_handle,SPI_HandleTypeDef* hspi,GPIO_TypeDef *pCS_gpio,uint16_t CS_PinNumber,uint8_t MCP_Mode){
+
 	Mcp_handle->CS_PinNumber = CS_PinNumber;
 	Mcp_handle->CS_gpio = pCS_gpio;
-	Mcp_handle->hspi = pspi;
+	Mcp_handle->hspi = hspi;
+	Mcp_handle->MCP_Mode = MCP_Mode;
+
 }
 
-void MCP_StartCommunication(MCP_Handler_t *Mcp_Handle){
+/********************************************************************
+ * @fn				-  MCP3008_StartCommunication
+ *
+ * @brief			- Reset the communication
+ *
+ * @param[in]		- Address of MCP_Handle (Declared in the application)
+ *
+ * @return 			- NONE
+ *
+ * @note			- NONE
+ */
+void MCP3008_StartCommunication(MCP_Handler_t *Mcp_Handle){
 	HAL_GPIO_WritePin(Mcp_Handle->CS_gpio, Mcp_Handle->CS_PinNumber, GPIO_PIN_RESET);
 }
 
-void MCP_StopCommunication(MCP_Handler_t *Mcp_Handle){
+
+/********************************************************************
+ * @fn				-  MCP3008_StopCommunication
+ *
+ * @brief			- Reset the communication
+ *
+ * @param[in]		- Address of MCP_Handle (Declared in the application)
+ *
+ * @return 			- NONE
+ *
+ * @note			- NONE
+ */
+void MCP3008_StopCommunication(MCP_Handler_t *Mcp_Handle){
 	HAL_GPIO_WritePin(Mcp_Handle->CS_gpio, Mcp_Handle->CS_PinNumber, GPIO_PIN_SET);
 }
 
 
-void MCP_ResetCommunication(MCP_Handler_t *Mcp_Handle){
+/********************************************************************
+ * @fn				- MCP3008_ResetCommunication
+ *
+ * @brief			- Reset the communication
+ *
+ * @param[in]		- Address of MCP_Handle (Declared in the application)
+ *
+ * @return 			- NONE
+ *
+ * @note			- NONE
+ */
+void MCP3008_ResetCommunication(MCP_Handler_t *Mcp_Handle){
 	HAL_GPIO_WritePin(Mcp_Handle->CS_gpio, Mcp_Handle->CS_PinNumber, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(Mcp_Handle->CS_gpio, Mcp_Handle->CS_PinNumber, GPIO_PIN_RESET);
 }
 
 
-uint16_t MCP_ReadChannel(MCP_Handler_t *Mcp_Handle, uint8_t channel){
+/********************************************************************
+ * @fn				- MCP3008_ReadChannel
+ *
+ * @brief			- Read a channel of the MCP
+ *
+ * @param[in]		- Address of MCP_Handle (Declared in the application)
+ * @param[in]		- channel (Chosen from @MCP_Channels)
+ *
+ * @return 			- Sampled data ( on 10 Bits)
+ *
+ * @note			- This function is specified for MCP3008
+ */
+uint16_t MCP3008_ReadChannel(MCP_Handler_t *Mcp_Handle, uint8_t channel){
 	uint8_t txData[3], rxData[3];
 	uint16_t adcResult;
 
-    txData[0] = 0x01;                              // Start bit
-    txData[1] = 0x80 | (channel << 4);             // SGL/DIFF = 1 (single-ended), Channel selection
-    txData[2] = 0x00;                              // Dummy byte for clock cycles
+	if (channel > 7) return 0;  // Invalid channel, return 0
+
+    txData[0] = 0x01;             								 // Start bit
+    txData[1] = (Mcp_Handle->MCP_Mode << 7) | (channel << 4);    // Mode selection and Channel selection
+    txData[2] = 0x00;                             				 // Dummy byte for clock cycles
 
 	//Start Communication
 	MCP_StartCommunication(Mcp_Handle);
 	// Full-Duplex transmission (send txData, receive rxData)
-	HAL_SPI_TransmitReceive(Mcp_Handle->hspi, txData, rxData, 3, HAL_MAX_DELAY);
+	if (HAL_SPI_TransmitReceive(Mcp_Handle->hspi, txData, rxData, 3, HAL_MAX_DELAY) != HAL_OK) {
+	        MCP_StopCommunication(Mcp_Handle);
+	        return 0; //SPI Error
+	    }
 	//Close Communication
 	MCP_StopCommunication(Mcp_Handle);
 
-	adcResult = ((uint16_t)rxData[1] << 8) | rxData[2]; //
-	adcResult &= 0x07FF; //Masking Bits
+	adcResult = ((uint16_t)rxData[1] << 8) | rxData[2];
+	adcResult &= 0x03FF; //Masking Bits
 
 	return adcResult;
 }
